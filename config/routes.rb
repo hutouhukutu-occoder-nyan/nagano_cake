c4d5a56ed042f402098a02fe17a5e805166f5740
EC2 Default User <ec2-user@ip-172-31-33-147.eu-north-1.compute.internal>
2024-11-17 02:53:42 +0000

    [add]public/cart_itemsコントローラー内メソッド、ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  
+  #cart_items
+  delete '/cart_items/all_destroy', to: 'public/cart_items#all_destroy', as: :cart_items_all_destroy
+  scope module: :public do
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+  end 
+
+
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: 'admin/sessions'
   }
