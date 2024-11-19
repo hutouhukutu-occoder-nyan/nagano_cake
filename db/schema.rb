63c7b19c6039e969ba840fd3c285996d84fbb58c
EC2 Default User <ec2-user@ip-172-31-44-255.eu-north-1.compute.internal>
2024-11-19 04:19:25 +0000

    [Add]items,addresses,genresのモデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,15 @@
 
 ActiveRecord::Schema.define(version: 2024_11_17_020518) do
 
+  create_table "addresses", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "address", null: false
+    t.string "postal_code", null: false
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
