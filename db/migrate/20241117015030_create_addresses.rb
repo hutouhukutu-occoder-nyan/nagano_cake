a9ada977c79a301af9ba058e17d5ef13039d448b
EC2 Default User <ec2-user@ip-172-31-34-246.ap-northeast-1.compute.internal>
2024-11-19 03:40:52 +0000

    [Update]Addressesテーブルのカラム名を修正

--- db/migrate/20241117015030_create_addresses.rb
+++ db/migrate/20241117015030_create_addresses.rb
@@ -3,7 +3,7 @@ class CreateAddresses < ActiveRecord::Migration[6.1]
     create_table :addresses do |t|
       t.integer :customer_id, null:false
       t.string :address, null:false
-      t.string :post_code, null:false
+      t.string :postal_code, null:false
       t.string :name, null:false
 
       t.timestamps
