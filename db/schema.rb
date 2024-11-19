9d06bf3cab0d806a92ad008d5a68b2f61587b547
EC2 Default User <ec2-user@ip-172-31-34-246.ap-northeast-1.compute.internal>
2024-11-19 03:50:41 +0000

    [Update]Adressesテーブルカラム名修正

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2024_11_17_020518) do
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
     t.string "address", null: false
-    t.string "post_code", null: false
+    t.string "postal_code", null: false
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
