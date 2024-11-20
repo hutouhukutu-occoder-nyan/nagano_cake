8ee0c374d0ed9398b32d4739c278ca154cf092a7
EC2 Default User <ec2-user@ip-172-31-34-246.ap-northeast-1.compute.internal>
2024-11-20 10:51:53 +0000

    [Update]testファイル修正

--- test/controllers/admin/genre_controller_test.rb
+++ test/controllers/admin/genre_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class GenreControllerTest < ActionDispatch::IntegrationTest
+class Admin::GenreControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
--- test/controllers/admin/items_controller_test.rb
+++ test/controllers/admin/items_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class AdressesControllerTest < ActionDispatch::IntegrationTest
+class Admin::ItemsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
