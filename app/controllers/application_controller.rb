41b10b34b420d81003305bf9ddfefffedb899bea
EC2 Default User <ec2-user@ip-172-31-44-255.eu-north-1.compute.internal>
2024-11-21 04:13:45 +0000

    マイページと退会画面とサインアップの不具合解消

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,14 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_up_path_for(resource)
-    case resource
-    when Admin
-      admin_path
-    when Customer
       root_path
-    else
-      root_path
-    end
   end
 
   def after_sign_out_path_for(resource)
--- db/schema.rb
+++ db/schema.rb
@@ -75,14 +75,14 @@ ActiveRecord::Schema.define(version: 2024_11_20_140306) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "last_name"
-    t.string "first_name"
-    t.string "last_name_kana"
-    t.string "first_name_kana"
-    t.string "post_code"
-    t.string "address"
-    t.string "telephone_number"
-    t.boolean "is_active"
+    t.string "last_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name_kana", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
+    t.string "postal_code", default: "", null: false
+    t.string "address", default: "", null: false
+    t.string "telephone_number", default: "", null: false
+    t.boolean "is_active", default: true, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
