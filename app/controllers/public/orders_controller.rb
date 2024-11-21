803d19f90d47554c362aa47c3cf27a74cb3034bc
EC2 Default User <ec2-user@ip-172-31-33-147.eu-north-1.compute.internal>
2024-11-21 04:12:45 +0000

    [add]orders_controllerのcheckアクション定義

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,6 +3,32 @@ class Public::OrdersController < ApplicationController
   end
 
   def check
+    @cart_items = CartItem.where(customer_id:current_customer.id)
+    @shipping_cost = 800
+    @peyment_method = params[:order][:patment_method]
+    @total_cart_price = current_customer.total_cart_price
+    @total_payment = @total_cart_price + @shipping_cost
+    @address = params[:order][:addess]
+    case @address
+    when "customer_address"
+      @selected_address = "〒" + current_customer.post_code + "" + curretn_customer.address
+      @selected_name = current_customer.last_name + current_customer.first_name
+    when "registred_address"
+      unless params[:order][:address_id] == ""
+        selected = Adress.find(params[:order][:address_id])
+        @selected_address = "〒" +selected.postal_code + "" + selected.address
+        @selected_name = selected.name
+      else
+        render :new
+      end
+    when "new_address"
+      unless params[:order][:new_postal_code] == "" && params[:order][:new_address] == "" && params[:order][:new_name] == ""
+        @selected_address = "〒" + params[:order][:new_postal_code] + "" + params[:order][:new_address]
+        @selected_name = params[:order][:new_name]
+      else
+        render :new
+      end
+    end
   end
 
   def complete
