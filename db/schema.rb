832d9aecf7fc429acad2f77d872c4456c18f0744
EC2 Default User <ec2-user@ip-172-31-44-255.eu-north-1.compute.internal>
2024-11-23 07:46:32 +0000

    マイグレーションしなおし

--- db/schema.rb
+++ db/schema.rb
@@ -62,8 +62,8 @@ ActiveRecord::Schema.define(version: 2024_11_20_140306) do
   end
 
   create_table "cart_items", force: :cascade do |t|
-    t.integer "customer_id"
-    t.integer "item_id"
+    t.integer "customer_id", null: false
+    t.integer "item_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "amount", null: false
